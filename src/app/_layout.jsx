4b9e557537545069a08d325fa01afccbc7c73054
shahzaib4587 <m.shahzaib@clustox.com>
2024-04-17 00:46:07 +0500

    added navigation depencies

--- src/app/_layout.jsx
+++ src/app/_layout.jsx
@@ -1,8 +1,11 @@
+import React from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { SessionProvider } from "../contexts/sessionContext";
 import { SafeAreaView, View } from "react-native";
-import { Stack } from "expo-router";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { NavigationContainer } from "@react-navigation/native"; // Import NavigationContainer
+import { Stack } from "@react-navigation/native"; // Import Stack for navigation
+
+const Stack = createStackNavigator(); // Create a Stack Navigator instance
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -19,10 +22,11 @@ export default function Layout() {
         <GestureHandlerRootView>
           <SafeAreaView>
             <View height="100%" collapsable={false}>
-              <Stack
-                initialRouteName="(app)"
-                screenOptions={{ header: () => null }}
-              />
+              <NavigationContainer>
+                <Stack.Navigator
+                  screenOptions={{ header: () => null }}
+                ></Stack.Navigator>
+              </NavigationContainer>
             </View>
           </SafeAreaView>
         </GestureHandlerRootView>
